689ae80b90a24682ac25cbf8180280e578a630be
Kamal Hamza <khamsa.kamal48@gmail.com>
2021-10-28 21:50:30 +0530

    Changed the dependency from pysftp to paramiko

--- Download and Update in Raisers Edge.py
+++ Download and Update in Raisers Edge.py
@@ -1,8 +1,32 @@
-import pysftp, json, requests
+import pysftp, json, requests, os, sys, paramiko
 
-with open('sftp_cred.json', 'r') as sftp_cred_file:
-    sftp_cred = json.load(sftp_cred_file)
-    host = sftp_cred['host']
-    port = sftp_cred['port']
-    username = sftp_cred['username']
-    password = sftp_cred['password']
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Set current directory
+os.getcwd()
+
+# Download file from SFTO Server
+HOST = os.getenv("HOST")
+PORT = int(os.getenv("PORT"))
+USERNAME = os.getenv("USERN")
+PASSWORD = os.getenv("PASSWORD")
+SOURCE = os.getenv("SOURCE_PATH")
+DESTINATION = os.getenv("DESTINATION_PATH")
+
+# Open a transport
+transport = paramiko.Transport((HOST,PORT))
+
+# Auth
+transport.connect(None,USERNAME,PASSWORD)
+
+# Connect Now
+sftp = paramiko.SFTPClient.from_transport(transport)
+
+# Download files
+sftp.get(SOURCE, DESTINATION)
+
+# Close
+if sftp: sftp.close()
+if transport: transport.close()
