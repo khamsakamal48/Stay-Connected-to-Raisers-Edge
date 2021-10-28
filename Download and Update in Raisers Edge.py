0d062c3b691f20ac26e6227cf811c29c033a338f
Kamal Hamza <khamsa.kamal48@gmail.com>
2021-10-29 00:40:57 +0530

    can merge csv files and remove duplicates

--- Download and Update in Raisers Edge.py
+++ Download and Update in Raisers Edge.py
@@ -1,4 +1,4 @@
-import pysftp, json, requests, os, sys
+import pysftp, json, requests, os, sys, csv, shutil, glob, pandas
 
 from dotenv import load_dotenv
 
@@ -30,5 +30,28 @@ with pysftp.Connection(host=HOST, port=PORT, username=USERNAME, password=PASSWOR
       # Remove file
       sftp.remove(each_file)
 
+# Merge CSV files into one
+os.system("touch Stay_Connected_Merged_All.csv")
+os.system("awk '(NR == 1) || (FNR > 1)' *-*.csv > Stay_Connected_Merged_All.csv")
+
+#Remove blank rows
+os.system("sed -i 's/\s*;\s*/;/g' Stay_Connected_Merged_All.csv")
+os.system("sed -i 's/\s*,\s*/,/g' Stay_Connected_Merged_All.csv")
+os.system("sed -i '/^$/d' Stay_Connected_Merged_All.csv")
+os.system("awk -F, 'length>NF+1' Stay_Connected_Merged_All.csv > Stay_Connected_Merged_All_New.csv")
+
+# Move files
+shutil.move("Stay_Connected_Merged_All_New.csv","../Stay_Connected_Merged_All.csv")
+
+# Delete all CSV files
+csv_files = glob.glob('*.csv')
+for each_csv_file in csv_files:
+  os.remove(each_csv_file)
+
 # Change Directory back
 os.chdir('..')
+
+# Remove duplicates
+merged_csv_file = pandas.read_csv('Stay_Connected_Merged_All.csv')
+merged_csv_file.drop_duplicates(inplace=True)
+merged_csv_file.to_csv('Stay_Connected_Merged_All.csv', index=False)
