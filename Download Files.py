8a8000fa154bb118107ba4706cb185b4b6dcbce0
khamsakamal48 <khamsa.kamal48@gmail.com>
2022-03-17 18:24:14 +0530

    Increase CSV max size limit

--- Download Files.py
+++ Download Files.py
@@ -2,6 +2,19 @@
 
 import pysftp, os, csv, shutil, glob, pandas, psycopg2, sys
 
+# Increase CSV Max size limit
+maxInt = sys.maxsize
+
+while True:
+    # decrease the maxInt value by factor 10 
+    # as long as the OverflowError occurs.
+
+    try:
+        csv.field_size_limit(maxInt)
+        break
+    except OverflowError:
+        maxInt = int(maxInt/10)
+
 # Set current directory
 os.chdir(os.path.dirname(sys.argv[0]))
 
