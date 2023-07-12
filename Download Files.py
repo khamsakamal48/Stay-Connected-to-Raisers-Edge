39af1fea53695adf3fd9f32cfe0b3cbd07f7bf2e
Kamal Hamza <khamsa.kamal48@gmail.com>
2023-07-12 16:00:48 +0530

    Initial version

--- Download Files.py
+++ Download Files.py
@@ -1,131 +1,366 @@
-#!/usr/bin/env python3
-
-import pysftp, os, csv, shutil, glob, pandas, psycopg2, sys
-
-# Increase CSV Max size limit
-maxInt = sys.maxsize
-
-while True:
-    # decrease the maxInt value by factor 10 
-    # as long as the OverflowError occurs.
-
-    try:
-        csv.field_size_limit(maxInt)
-        break
-    except OverflowError:
-        maxInt = int(maxInt/10)
-
-# Set current directory
-#os.chdir(os.path.dirname(sys.argv[0]))
-os.chdir(os.getcwd())
+import pandas as pd
+import pysftp
+import os
+import shutil
+import glob
+import pandas
+import logging
+import smtplib
+import ssl
+import imaplib
+import datetime
+import time
+import numpy as np
+import phonenumbers
 
 from dotenv import load_dotenv
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from jinja2 import Environment
+from datetime import datetime
+from email_validator import validate_email, EmailNotValidError
 
-load_dotenv()
+def set_directory():
+    global owd
 
-# Create a .env file with below parameters
-#HOST=
-#PORT=
-#USERN=
-#PASSWORD=
-#SOURCE_PATH=
-#DESTINATION_PATH=
-#DB_IP=
-#DB_NAME=
-#DB_USERNAME=
-#DB_PASSWORD=
-#AUTH_CODE=
-#REDIRECT_URL=
-#CLIENT_ID=
-#RE_API_KEY=
-#MAIL_USERN=
-#MAIL_PASSWORD=
-#IMAP_URL=
-#IMAP_PORT=
-#SMTP_URL=
-#SMTP_PORT=
+    owd = os.getcwd()
+    os.chdir(owd)
 
-# Retrieve contents from .env file
-HOST = os.getenv("HOST")
-PORT = int(os.getenv("PORT")) # Change port from string to integer
-USERNAME = os.getenv("USERN")
-PASSWORD = os.getenv("PASSWORD")
-SOURCE = os.getenv("SOURCE_PATH")
-DESTINATION = os.getenv("DESTINATION_PATH")
-DB_IP = os.getenv("DB_IP")
-DB_NAME = os.getenv("DB_NAME")
-DB_USERNAME = os.getenv("DB_USERNAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+def load_env():
+    logging.info('Setting Environment variables')
 
-# Change Directory 
-os.chdir(DESTINATION)
+    global HOST, PORT, USERNAME, PASSWORD, SOURCE, MAIL_USERN, MAIL_PASSWORD, IMAP_URL, IMAP_PORT, SMTP_URL, SMTP_PORT, SEND_TO
 
-# Connect to Download files from SFTP Server
-with pysftp.Connection(host=HOST, port=PORT, username=USERNAME, password=PASSWORD) as sftp:
-  # Change directory in remote
-  with sftp.cd(SOURCE):
-    # List all files in that directory
-    files = sftp.listdir()
-    for each_file in files:
-      # Download file
-      sftp.get(each_file)
-      # Remove file
-      sftp.remove(each_file)
+    load_dotenv()
 
-# Merge CSV files into one
-os.system("awk '(NR == 1) || (FNR > 1)' *-*.csv > Stay_Connected_Merged.csv")
+    # Retrieve contents from .env file
+    HOST = os.getenv("HOST")
+    PORT = int(os.getenv("PORT"))  # Change port from string to integer
+    USERNAME = os.getenv("USERN")
+    PASSWORD = os.getenv("PASSWORD")
+    SOURCE = os.getenv("SOURCE_PATH")
+    MAIL_USERN = os.getenv('MAIL_USERN')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    IMAP_URL = os.getenv('IMAP_URL')
+    IMAP_PORT = os.getenv('IMAP_PORT')
+    SMTP_URL = os.getenv('SMTP_URL')
+    SMTP_PORT = os.getenv('SMTP_PORT')
+    SEND_TO = os.getenv('SEND_TO')
 
-#Remove blank rows
-os.system("sed -i 's/\s*;\s*/;/g' Stay_Connected_Merged.csv")
-os.system("sed -i 's/\s*,\s*/,/g' Stay_Connected_Merged.csv")
-os.system("sed -i '/^$/d' Stay_Connected_Merged.csv")
-os.system("awk -F, 'length>NF+1' Stay_Connected_Merged.csv > Stay_Connected_Merged_All_New.csv")
+def start_logging():
+    global process_name
 
-# Delete last column from CSV file
-with open("Stay_Connected_Merged_All_New.csv", "r") as fin:
-    with open("Stay_Connected_Merged_All.csv", "w") as fout:
-        writer=csv.writer(fout)
-        for row in csv.reader(fin):
-            writer.writerow(row[:-1])
+    # Get File Name of existing script
+    process_name = os.path.basename(__file__).replace('.py', '').replace(' ', '_')
 
-# Move files
-shutil.move("Stay_Connected_Merged_All.csv","../Stay_Connected_Merged_All.csv")
+    logging.basicConfig(filename=f'Logs/{process_name}.log', format='%(asctime)s %(levelname)s %(process)d %(message)s', filemode='w', level=logging.INFO)
 
-# Delete all CSV files
-csv_files = glob.glob('*.csv')
-for each_csv_file in csv_files:
-  os.remove(each_csv_file)
+    # Printing the output to file for debugging
+    logging.info('Starting the Script')
 
-# Change Directory back
-os.chdir('..')
+def stop_logging():
+    logging.info('Stopping the Script')
 
-# Remove duplicates
-merged_csv_file = pandas.read_csv('Stay_Connected_Merged_All.csv')
-merged_csv_file.drop_duplicates(inplace=True)
-merged_csv_file.to_csv('Stay_Connected_Merged_All.csv', index=False)
+def housekeeping():
+    logging.info('Doing Housekeeping')
 
-# PostgreSQL DB Connection
-conn = psycopg2.connect(host=DB_IP, dbname=DB_NAME, user=DB_USERNAME, password=DB_PASSWORD)
+    # Change Directory
+    os.chdir('Files')
 
-# Open connection
-cur = conn.cursor()
+    # Housekeeping
+    multiple_files = glob.glob('*.csv')
 
-# Delete rows in table
-cur.execute("truncate updates_from_stayconnected;")
+    # Iterate over the list of filepaths & remove each file.
+    logging.info('Removing old CSV files')
+    for each_file in multiple_files:
+        try:
+            os.remove(each_file)
+        except:
+            pass
 
-# Commit changes
-conn.commit()
+    # Change Directory
+    os.chdir(owd)
 
-# Copying contents of CSV file to PostgreSQL DB
-with open('Stay_Connected_Merged_All.csv', 'r') as input_csv:
+def connect_to_sftp():
+    logging.info('Connecting to SFTP Server')
 
-    # Skip the header row.
-    next(input_csv)
-    cur.copy_from(input_csv, 'updates_from_stayconnected', sep=',')
+    # Change Directory
+    os.chdir('Files')
 
-# Commit changes
-conn.commit()
+    # Connect to Download files from SFTP Server
+    with pysftp.Connection(host=HOST, port=PORT, username=USERNAME, password=PASSWORD) as sftp:
+        # Change directory in remote
+        with sftp.cd(SOURCE):
+            # List all files in that directory
+            files = sftp.listdir()
+            logging.info('Downloading files from server')
+            for each_file in files:
+                # Download file
+                sftp.get(each_file)
+                # Remove file
+                # sftp.remove(each_file)
 
-# Close DB connection
-cur.close()
-conn.close()
+    # Change Directory
+    os.chdir(owd)
+
+def create_dataframe():
+    logging.info('Creating a Dataframe of the Stay Connected files')
+
+    multiple_files = glob.glob('Files/*.csv')
+
+    data = pd.DataFrame()
+
+    # Iterate over the list of filepaths & load each file.
+    for each_file in multiple_files:
+        df = pd.read_csv(each_file, on_bad_lines='skip', engine='python')
+        data = pd.concat([data, df])
+
+    data = data.drop_duplicates().reset_index(drop=True).copy()
+
+    # Data Processing
+    data = pre_process(data).copy()
+
+    # Save to a file
+    data.drop_duplicates().reset_index(drop=True).to_parquet('Database/Stay Connected.parquet', index=False)
+
+    # Change Directory
+    os.chdir(owd)
+
+def pre_process(data):
+    # Remove column
+    data = data.drop(columns=['Unnamed: 27']).copy()
+
+    # Country code
+    data['countrycode'] = data['countrycode'].fillna(0)
+    data['countrycode'] = data['countrycode'].astype(int)
+    data['countrycode'] = data['countrycode'].replace(0, '')
+
+    # Unnecessary Email columns
+    data = data.drop(columns=['email1', 'email2', 'email3', 'email4', 'email5']).copy()
+
+    # Emails
+    data['email'] = data['email'].apply(lambda x: check_email(x))
+    data['alternateemail'] = data['alternateemail'].apply(lambda x: check_email(x))
+
+    # Country code
+    data['countrycode'] = data['countrycode'].replace('', np.NaN)
+    data['countrycode'] = data[['country', 'contact', 'countrycode']].apply(lambda x: add_missing_country_code(*x),
+                                                                            axis=1)
+    data['countrycode'] = data['countrycode'].fillna(0)
+    data['countrycode'] = data['countrycode'].astype(int)
+
+    # Phone
+    data['phone'] = data['countrycode'].replace(0, '').astype(str) + data['contact'].astype(str)
+    data['phone'] = data['phone'].replace('nan', np.NaN)
+
+    # Remove columns
+    data = data.drop(columns=['countrycode', 'contact']).copy()
+
+    # Phones
+    data['phone'] = data['phone'].astype(str)
+    data['phone'] = data[['phone', 'country']].apply(lambda x: check_phone(*x), axis=1)
+
+    # Batch
+    data['batch'] = data['batch'].fillna(0)
+    data['batch'] = data['batch'].astype(int)
+
+    # Department
+    data['dept'] = data['dept'].replace('Other', np.NaN)
+
+    # Position
+    data['position'] = data['position'].apply(lambda x: str(x)[-50:])
+    data['position'] = data['position'].astype(str).replace('nan', np.NaN)
+
+    # Clean LinkedIn URL
+    data['linkedIn'] = data['linkedIn'].apply(lambda x: clean_linkedin(x))
+
+    # Drop Columns
+    data = data.drop(columns=['status', 'created_on', 'interest', 'concentdetails']).copy()
+
+    return data
+
+def check_email(email):
+    email = str(email).strip()
+
+    try:
+        # Validate.
+        valid = validate_email(email, check_deliverability=False, dns_resolver=None, allow_quoted_local=True)
+
+        # Update with the normalized form.
+        email = valid.email
+        return email
+    except: return np.NaN
+
+def add_missing_country_code(country, phone, code):
+    if country == 'India' and pd.isnull(code) and len(str(phone)) == 10: return 91
+    else: return code
+
+def check_phone(phone, country):
+    phone = ''.join(filter(str.isdigit, phone))
+
+    if country == 'India': phone = str(phone)[-12:]
+
+    try:
+        phone = '+' + str(phone).replace('+','')
+        phone = phonenumbers.parse(phone)
+
+        if phonenumbers.is_possible_number(phone): return phonenumbers.format_number(phone, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+        else: return np.NaN
+    except: return np.NaN
+
+def clean_linkedin(url):
+    url = str(url)
+
+    if 'linkedin' in url:
+        url = url.replace('https://', '').replace('http://', '').replace('www.', '')
+        url = url.split('?', 1)[0]
+        if url[-1] == '/': url = url[0:len(url)-1]
+        return url
+    else: return np.NaN
+
+def send_error_emails(subject):
+    logging.info('Sending email for an error')
+
+    message = MIMEMultipart()
+    message["Subject"] = subject
+    message["From"] = MAIL_USERN
+    message["To"] = SEND_TO
+
+    # Adding Reply-to header
+    message.add_header('reply-to', MAIL_USERN)
+
+    TEMPLATE = """
+    <table style="background-color: #ffffff; border-color: #ffffff; width: auto; margin-left: auto; margin-right: auto;">
+    <tbody>
+    <tr style="height: 127px;">
+    <td style="background-color: #363636; width: 100%; text-align: center; vertical-align: middle; height: 127px;">&nbsp;
+    <h1><span style="color: #ffffff;">&nbsp;Raiser's Edge Automation: {{job_name}} Failed</span>&nbsp;</h1>
+    </td>
+    </tr>
+    <tr style="height: 18px;">
+    <td style="height: 18px; background-color: #ffffff; border-color: #ffffff;">&nbsp;</td>
+    </tr>
+    <tr style="height: 18px;">
+    <td style="width: 100%; height: 18px; background-color: #ffffff; border-color: #ffffff; text-align: center; vertical-align: middle;">&nbsp;<span style="color: #455362;">This is to notify you that execution of Auto-updating Alumni records has failed.</span>&nbsp;</td>
+    </tr>
+    <tr style="height: 18px;">
+    <td style="height: 18px; background-color: #ffffff; border-color: #ffffff;">&nbsp;</td>
+    </tr>
+    <tr style="height: 61px;">
+    <td style="width: 100%; background-color: #2f2f2f; height: 61px; text-align: center; vertical-align: middle;">
+    <h2><span style="color: #ffffff;">Job details:</span></h2>
+    </td>
+    </tr>
+    <tr style="height: 52px;">
+    <td style="height: 52px;">
+    <table style="background-color: #2f2f2f; width: 100%; margin-left: auto; margin-right: auto; height: 42px;">
+    <tbody>
+    <tr>
+    <td style="width: 50%; text-align: center; vertical-align: middle;">&nbsp;<span style="color: #ffffff;">Job :</span>&nbsp;</td>
+    <td style="background-color: #ff8e2d; width: 50%; text-align: center; vertical-align: middle;">&nbsp;{{job_name}}&nbsp;</td>
+    </tr>
+    <tr>
+    <td style="width: 50%; text-align: center; vertical-align: middle;">&nbsp;<span style="color: #ffffff;">Failed on :</span>&nbsp;</td>
+    <td style="background-color: #ff8e2d; width: 50%; text-align: center; vertical-align: middle;">&nbsp;{{current_time}}&nbsp;</td>
+    </tr>
+    </tbody>
+    </table>
+    </td>
+    </tr>
+    <tr style="height: 18px;">
+    <td style="height: 18px; background-color: #ffffff;">&nbsp;</td>
+    </tr>
+    <tr style="height: 18px;">
+    <td style="height: 18px; width: 100%; background-color: #ffffff; text-align: center; vertical-align: middle;">Below is the detailed error log,</td>
+    </tr>
+    <tr style="height: 217.34375px;">
+    <td style="height: 217.34375px; background-color: #f8f9f9; width: 100%; text-align: left; vertical-align: middle;">{{error_log_message}}</td>
+    </tr>
+    </tbody>
+    </table>
+    """
+
+    # Create a text/html message from a rendered template
+    emailbody = MIMEText(
+        Environment().from_string(TEMPLATE).render(
+            job_name=subject,
+            current_time=datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+            error_log_message=Argument
+        ), "html"
+    )
+
+    # Add HTML parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    try:
+        message.attach(emailbody)
+        attach_file_to_email(message, f'Logs/{process_name}.log')
+        emailcontent = message.as_string()
+
+    except:
+        message.attach(emailbody)
+        emailcontent = message.as_string()
+
+    # Create secure connection with server and send email
+    context = ssl._create_unverified_context()
+    with smtplib.SMTP_SSL(SMTP_URL, SMTP_PORT, context=context) as server:
+        server.login(MAIL_USERN, MAIL_PASSWORD)
+        server.sendmail(
+            MAIL_USERN, SEND_TO, emailcontent
+        )
+
+    # Save copy of the sent email to sent items folder
+    with imaplib.IMAP4_SSL(IMAP_URL, IMAP_PORT) as imap:
+        imap.login(MAIL_USERN, MAIL_PASSWORD)
+        imap.append('Sent', '\\Seen', imaplib.Time2Internaldate(time.time()), emailcontent.encode('utf8'))
+        imap.logout()
+
+def attach_file_to_email(message, filename):
+    logging.info('Attach file to email')
+
+    # Open the attachment file for reading in binary mode, and make it a MIMEApplication class
+    with open(filename, "rb") as f:
+        file_attachment = MIMEApplication(f.read())
+
+    # Add header/name to the attachments
+    file_attachment.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename.replace('Logs', '')}",
+    )
+
+    # Attach the file to the message
+    message.attach(file_attachment)
+
+try:
+    # Set current directory
+    set_directory()
+
+    # Start Logging
+    start_logging()
+
+    # Load ENV
+    load_env()
+
+    # Housekeeping
+    housekeeping()
+
+    # Connect to SFTP Server and Download files
+    connect_to_sftp()
+
+    # Merge to a single CSV
+    create_dataframe()
+
+except Exception as Argument:
+    logging.error(Argument)
+
+    # Send emails
+    send_error_emails('Downloading files for Stay Connected updates')
+
+finally:
+    # Housekeeping
+    housekeeping()
+
+    # Stop Logging
+    stop_logging()
+
+    exit()
--- README.md
+++ README.md
@@ -1,97 +1,36 @@
 # Stay Connected to Raisers Edge
 
+## Install below packages
+```shell
 sudo apt install python3-pip
-sudo apt install csvkit
 
-pip3 install pandas
-pip3 install pysftp
-pip3 install psycopg2
-pip3 install python-dotenv
-pip3 install fuzzywuzzy
-pip3 install python-Levenshtein
-
-
-If you encounter error on installing pyscopg2, then try:
-
-```bash
-pip3 install psycopg2-binary
+# Install Python packages
+pip install pandas
+pip install pysftp
+pip install python-dotenv
+pip install fuzzywuzzy
+pip install python-Levenshtein
+pip install email-validator
+pip install phonenumbers
+pip install pyarrow
+pip install fastparquet
 ```
 
-Install PostgreSQL
-
-Create stay-connected database
-CREATE DATABASE "stay-connected";
-
-Create tables
-
-CREATE TABLE updates_from_stayconnected
-(
-    id character varying,
-    fname character varying,
-    lname character varying,
-    email character varying,
-    email1 character varying,
-    email2 character varying,
-    email3 character varying,
-    email4 character varying,
-    email5 character varying,
-    contact character varying,
-    batch character varying,
-    dept character varying,
-    hostel character varying,
-    country character varying,
-    state character varying,
-    city character varying,
-    organization character varying,
-    position character varying,
-    status character varying,
-    created_on character varying,
-    interest character varying
-);
-
-CREATE TABLE updated_in_raisers_edge
-(
-    id character varying,
-    fname character varying,
-    lname character varying,
-    email character varying,
-    email1 character varying,
-    email2 character varying,
-    email3 character varying,
-    email4 character varying,
-    email5 character varying,
-    contact character varying,
-    batch character varying,
-    dept character varying,
-    hostel character varying,
-    country character varying,
-    state character varying,
-    city character varying,
-    organization character varying,
-    position character varying,
-    status character varying,
-    created_on character varying,
-    interest character varying
-);
-
-# Create a .env file with below parameters
-#HOST=
-#PORT=
-#USERN=
-#PASSWORD=
-#SOURCE_PATH=
-#DESTINATION_PATH=
-#DB_IP=
-#DB_NAME=
-#DB_USERNAME=
-#DB_PASSWORD=
-#AUTH_CODE=
-#REDIRECT_URL=
-#CLIENT_ID=
-#RE_API_KEY=
-#MAIL_USERN=
-#MAIL_PASSWORD=
-#IMAP_URL=
-#IMAP_PORT=
-#SMTP_URL=
-#SMTP_PORT=
+## Create a .env file with below parameters
+```shell
+HOST=
+PORT=
+USERN=
+PASSWORD=
+SOURCE_PATH=
+AUTH_CODE=
+REDIRECT_URL=
+CLIENT_ID=
+RE_API_KEY=
+MAIL_USERN=
+MAIL_PASSWORD=
+IMAP_URL=
+IMAP_PORT=
+SMTP_URL=
+SMTP_PORT=
+```
